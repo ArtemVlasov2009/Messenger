e6fee2ff8ec8f374b3b8efd76d73acbc21eb002d
Bogdantkach12 <rudy.bodik@gmail.com>
2025-05-17 10:02:07 +0300

    modal

---
+++ Messenger/Messenger_App/views.py
@@ -0,0 +1,21 @@
+from django.shortcuts import render, redirect
+from .forms import AuthorizationForm
+
+# Create your views here.
+def render_messenger_page(request):
+    return render(request, 'Messenger_App/Messenger.html', {'current_page': 'home'})
+
+def render_authorization_page(request):
+    if request.method == 'POST':
+        form = AuthorizationForm(request.POST)
+        if form.is_valid():
+            # тут логика авторизации
+            email = form.cleaned_data['email']
+            password = form.cleaned_data['password']
+            verification = form.cleaned_data['email_verification']
+            # ...
+            return redirect('/')
+    else:
+        form = AuthorizationForm()
+
+    return render(request, 'Messenger_App/authorization.html', {'form': form})
--- Posts_app/forms.py
+++ Posts_app/forms.py
@@ -1,8 +1,29 @@
 from django import forms
 from .models import User_Post
 
+from django import forms
+from .models import User_Post
+
 class PostForm(forms.ModelForm):
     class Meta:
         model = User_Post
-        fields = ['title', 'theme', 'tags', 'text', 'article_link', 'image1', 'image2', 'image3', 'image4', 'image5', 'image6', 'image7', 'image8', 'image9']
-        
+        fields = ['title', 'theme', 'text', 'article_link']
+        widgets = {
+            'title': forms.TextInput(attrs={
+                'placeholder': 'Назва публікації',
+                'class': 'form-control'
+            }),
+            'theme': forms.TextInput(attrs={
+                'placeholder': 'Напишіть тему публікації',
+                'class': 'form-control'
+            }),
+            'text': forms.Textarea(attrs={
+                'placeholder': 'Поділіться своїми думками...',
+                'class': 'form-control',
+                'rows': 5
+            }),
+            'article_link': forms.URLInput(attrs={
+                'placeholder': 'https://www.instagram.com/...',
+                'class': 'form-control'
+            }),
+        }
